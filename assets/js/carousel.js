d2e6fdb51b1e61eab42f9ccd8d3cebac87ccf840
Re9iNee <re9inee@acc-pc1.mahak.com>
2020-09-10 11:56:02 +0430

    finilizing Projie

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,38 +1,8 @@
-console.group('carousel.js')
-// $('.carousel').carousel('pause');
-// $('.carousel').on('slide.bs.carousel', function () {
-//     console.error("slide");
-// })
-// $('.carousel').on('slid.bs.carousel', function () {
-//     console.error("sleeed");
-// })
-// $('.carousel').carousel('pause');
-// const slides = ["بازی", "معرفی"];
-// let currentSIndex, prevSIndex, nextSIndex;
-// currentSIndex = 0;
-// prevSIndex = currentSIndex == 0 ? slides.length - 1 : currentSIndex - 1;
-// nextSIndex = currentSIndex < slides.length ? currentSIndex + 1 : 0;
-// setInterval(() => {
-//     $('#prev').html(slides[prevSIndex]);
-//     $('#next').html(slides[nextSIndex]);
-//     $('.carousel').carousel('next');
-//     debugger
-//     if (currentSIndex < slides.length - 1) {
-//         increment()
-//     } else {
-//         reset()
-//     }
-// }, 2000);
+console.group('carousel.js');
+$(document).ready(function () {
+    // Activate Carousel
+    // console.log("Activated")
 
-
-// function reset() {
-//     currentSIndex = 0;
-//     prevSIndex = currentSIndex == 0 ? slides.length - 1 : currentSIndex - 1;
-//     nextSIndex = currentSIndex < slides.length ? currentSIndex + 1 : 0;
-// }
-
-// function increment() {
-//     prevSIndex = currentSIndex;
-//     nextSIndex = ++currentSIndex;
-// }
+    $("#carouselExampleIndicators").carousel();
+  });
 console.groupEnd()
